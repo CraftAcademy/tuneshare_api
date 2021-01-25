7da085789552154eabd3be1bc8bbd5e3890f9ce4
Kyungin Na <nakyungin93@gmail.com>
2021-01-25 15:25:05 +0100

    Creating a model spec for Post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
-
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'is expected to have db columns' do
+    it { is_expected.to have_db_column :track_name }
+    it { is_expected.to have_db_column :artists }
+    it { is_expected.to have_db_column :image }
+    it { is_expected.to have_db_column :preview_url }
+    it { is_expected.to have_db_column :description }
+  end
+
+  describe 'Factory Bot' do
+    it 'is expected to be valid' do
+      expect(create(:post)).to be_valid
+    end
+  end
 end
