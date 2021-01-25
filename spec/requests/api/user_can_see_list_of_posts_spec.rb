965d9420d4fd23dcc41359bd22796b8c7133a5f1
Kyungin Na <nakyungin93@gmail.com>
2021-01-25 14:46:39 +0100

    Creating user_can_see_list_of_posts_spec Creating response json module Adding get route for post Generating posts controller

---
+++ spec/requests/api/user_can_see_list_of_posts_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe 'GET /api/posts', type: :request do
+  describe 'successfully get a list of posts' do
+    before do
+      get '/api/posts'
+    end
+
+    it 'is expected to return a 200 response' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'is expected to return all posts'do
+      expect(response_json["posts"].count).to eq 3
+    end
+  end
+end
