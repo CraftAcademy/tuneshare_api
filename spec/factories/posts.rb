50014823d63abfa1842e298f77db06aeb555168e
Sanne <sannelindholm@hotmail.com>
2021-01-25 14:53:25 +0100

    Generating Post model with attributes

---
+++ spec/factories/posts.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :post do
+    track_name { "MyString" }
+    artists { "MyString" }
+    image { "MyText" }
+    preview_url { "MyText" }
+    description { "MyText" }
+  end
+end
