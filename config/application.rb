b78678db8e0b9ddf69188373dd41f1704928e529
viktor <viktorkarlsson125@gmail.com>
2021-01-25 10:52:57 +0100

    scaffolding Ruby on Rails application. deleting comment. changing ruby version to 2.5.1

---
+++ config/application.rb
@@ -0,0 +1,31 @@
+require_relative 'boot'
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+
+Bundler.require(*Rails.groups)
+
+module TuneshareApi
+  class Application < Rails::Application
+    config.load_defaults 6.0
+    config.api_only = true
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+      generate.request_specs false
+    end
+  end
+end
