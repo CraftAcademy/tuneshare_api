336d0bb5a95de071c7ae1eb9d28384ba92f4dd41
Sanne <sannelindholm@hotmail.com>
2021-01-25 15:03:32 +0100

    Migrating, creating index action, creating FactoryBot in test.

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,4 +1,6 @@
 class Api::PostsController < ApplicationController
   def index
+    posts = Post.all
+    render json: {posts: posts} 
   end
 end
--- spec/requests/api/user_can_see_list_of_posts_spec.rb
+++ spec/requests/api/user_can_see_list_of_posts_spec.rb
@@ -1,4 +1,5 @@
 RSpec.describe 'GET /api/posts', type: :request do
+  let!(:post) { 3.times { create(:post) } }
   describe 'successfully get a list of posts' do
     before do
       get '/api/posts'
@@ -8,8 +9,8 @@ RSpec.describe 'GET /api/posts', type: :request do
       expect(response).to have_http_status 200
     end
 
-    it 'is expected to return all posts'do
-      expect(response_json["posts"].count).to eq 3
+    it 'is expected to return all posts' do
+      expect(response_json['posts'].count).to eq 3
     end
   end
 end
