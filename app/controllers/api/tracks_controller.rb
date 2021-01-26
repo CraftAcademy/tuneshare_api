3e9b3a5b0709547ba1cb3796bc84af11d4d63c94
Kyungin Na <nakyungin93@gmail.com>
2021-01-26 11:01:26 +0100

    Formatting code, Modifying tracks controller

--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -1,13 +1,13 @@
 class Api::TracksController < ApplicationController
-  def index 
-    if params[:query] = params["q"]
-    spotify_tracks = RSpotify::Track.search(params[:q])
-    @tracks = spotify_tracks.map do |spotify_track|
-      format_response(spotify_track)
+  def index
+    if params[:query] = params['q']
+      spotify_tracks = RSpotify::Track.search(params[:q])
+      tracks = spotify_tracks.map do |spotify_track|
+        format_response(spotify_track)
+      end
     end
+    render json: { tracks: tracks }
   end
-    render json: { tracks: @tracks}
-  end 
 
   private
 
@@ -15,7 +15,7 @@ class Api::TracksController < ApplicationController
     {
       name: spotify_track.name,
       artists: spotify_track.artists[0].name,
-      image: spotify_track.album.images[0]["url"],
+      image: spotify_track.album.images[0]['url'],
       preview: spotify_track.preview_url
 
     }
--- spec/requests/api/user_can_search_for_a_song_to_post_spec.rb
+++ spec/requests/api/user_can_search_for_a_song_to_post_spec.rb
@@ -13,9 +13,7 @@ RSpec.describe 'GET /api/tracks', type: :request do
     end
 
     it 'is expected to return a name of the song ' do
-      expect(
-        response_json['tracks'][0]['name']
-      ).to eq 'All I Want for Christmas Is You'
+      expect(response_json['tracks'][0]['name']).to eq 'All I Want for Christmas Is You'
     end
 
     it 'is expected to return an artist name of the song ' do
@@ -23,15 +21,11 @@ RSpec.describe 'GET /api/tracks', type: :request do
     end
 
     it 'is expected to return an image of the song album' do
-      expect(
-        response_json['tracks'][0]['image']
-      ).to eq 'https://i.scdn.co/image/ab67616d0000b2734246e3158421f5abb75abc4f'
+      expect(response_json['tracks'][0]['image']).to eq 'https://i.scdn.co/image/ab67616d0000b2734246e3158421f5abb75abc4f'
     end
 
     it 'is expected to return a preview url of the song' do
-      expect(
-        response_json['tracks'][0]['preview']
-      ).to eq 'https://p.scdn.co/mp3-preview/bbafd15ff484394a0ca106d5fef0a81eeea4ef5b?cid=9165f2ed52ac4632b2c23038c2fbe1d9'
+      expect(response_json['tracks'][0]['preview']).to eq 'https://p.scdn.co/mp3-preview/bbafd15ff484394a0ca106d5fef0a81eeea4ef5b?cid=9165f2ed52ac4632b2c23038c2fbe1d9'
     end
   end
 end
