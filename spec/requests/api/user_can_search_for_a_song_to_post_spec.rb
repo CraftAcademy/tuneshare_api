193e5af209a627b62696960a157de2f1ef4076b8
Rithreaksa <wonderfulreaksa@gmail.com>
2021-01-26 10:28:45 +0100

    Adds Tracks controller Adds route for track Adds and modifies test for user can search for a song

--- spec/requests/api/user_can_search_for_a_song_to_post_spec.rb
+++ spec/requests/api/user_can_search_for_a_song_to_post_spec.rb
@@ -1,12 +1,7 @@
-RSpec.describe 'GET /api/posts', type: :request do
+RSpec.describe 'GET /api/tracks', type: :request do
   describe 'successfully get a list of songs' do
     before do
-      get '/api/posts',
-          params: {
-            query: 'All I Want for Christmas Is You'
-          }
-      @search_results = RSpotify::Track.search('All I Want for Christmas Is You')
-      @track = @search_results[0]
+      get '/api/tracks', params: { q: 'All I Want for Christmas Is You' }
     end
 
     it 'is expected to return 200 status' do
@@ -14,23 +9,29 @@ RSpec.describe 'GET /api/posts', type: :request do
     end
 
     it 'is expected to return a list of songs that contain query param' do
-      expect(@search_results.count).to eq 20
+      expect(response_json['tracks'].count).to eq 20
     end
 
     it 'is expected to return a name of the song ' do
-      expect(@track.name).to eq 'All I Want for Christmas Is You'
+      expect(
+        response_json['tracks'][0]['name']
+      ).to eq 'All I Want for Christmas Is You'
     end
 
     it 'is expected to return an artist name of the song ' do
-      expect(@track.artists[0].name).to eq 'Mariah Carey'
+      expect(response_json['tracks'][0]['artists']).to eq 'Mariah Carey'
     end
 
     it 'is expected to return an image of the song album' do
-      expect(@track.album.images[0]['url']).to eq 'https://i.scdn.co/image/ab67616d0000b2734246e3158421f5abb75abc4f'
+      expect(
+        response_json['tracks'][0]['image']
+      ).to eq 'https://i.scdn.co/image/ab67616d0000b2734246e3158421f5abb75abc4f'
     end
 
     it 'is expected to return a preview url of the song' do
-      expect(@track.preview_url).to eq 'https://p.scdn.co/mp3-preview/bbafd15ff484394a0ca106d5fef0a81eeea4ef5b?cid=9165f2ed52ac4632b2c23038c2fbe1d9'
+      expect(
+        response_json['tracks'][0]['preview']
+      ).to eq 'https://p.scdn.co/mp3-preview/bbafd15ff484394a0ca106d5fef0a81eeea4ef5b?cid=9165f2ed52ac4632b2c23038c2fbe1d9'
     end
   end
 end
