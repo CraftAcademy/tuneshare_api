6a51312f05af8b10689aaef86909d9907713c261
viktor <viktorkarlsson125@gmail.com>
2021-01-28 12:02:30 +0100

    deleting unfiniched lines from login spec

--- spec/requests/api/user_can_login_with_spotify_spec.rb
+++ spec/requests/api/user_can_login_with_spotify_spec.rb
@@ -1,9 +1,8 @@
 RSpec "api/auth" type: :request do
   describe 'successfully login' do
-    let(:user) {create (:user)}
     
     is "is expected to find user with correct attributes" do
-      expect(user)
+      
     end
   end
 end
