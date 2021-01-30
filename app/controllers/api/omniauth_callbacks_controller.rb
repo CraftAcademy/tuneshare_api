01feb7c59a1ea300bac82e7002a71db8570a79c2
Thomas <thomas@craftacademy.se>
2021-01-30 08:55:26 +0100

    manually tested solution for spotify auth

--- app/controllers/api/omniauth_callbacks_controller.rb
+++ app/controllers/api/omniauth_callbacks_controller.rb
@@ -1,61 +1,67 @@
 # frozen_string_literal: true
 
-module Api
-  class OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
-    include Devise::Controllers::Rememberable
+class Api::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
+  def omniauth_success
+    get_resource_from_auth_hash
+    set_token_on_resource
+    create_auth_params
+    sign_in(:user, @resource, store: false, bypass: false)
 
-    def omniauth_success
-      get_resource_from_auth_hash
-      set_token_on_resource
-      create_auth_params
-      sign_in(:user, @resource, store: false, bypass: false)
+    @resource.save!
+    response.set_header('spotify_credentials', auth_hash[:credentials]) # not sure about this at all
+    # DTA should do this for us?
+    response.set_header('access-token', auth_params[:auth_token])
+    response.set_header('token-type', 'Bearer')
+    response.set_header('client', auth_params[:client_id])
+    response.set_header('expiry', auth_params[:expiry])
+    response.set_header('uid', auth_params[:uid])
 
-      @resource.save!
+    render json: { user: @resource, spotify_info: auth_hash['info'] }
+  end
 
-      response.set_header('access-token', auth_params[:auth_token])
-      response.set_header('token-type', 'Bearer')
-      response.set_header('client', auth_params[:client_id])
-      response.set_header('expiry', auth_params[:expiry])
-      response.set_header('uid', auth_params[:uid])
-      response.set_header('spotify_credentials', auth_hash.credentials) #not sure about this at all
+  def validate_auth_origin_url_param
+    return render_error_not_allowed_auth_origin_url if auth_origin_url && blacklisted_redirect_url?(auth_origin_url)
+  end
 
-      render json: { user: @resource, spotify_info: auth_hash.info }
+  protected
+
+  def get_redirect_route(_devise_mapping)
+    path = "/auth/#{params[:provider]}/callback"
+    klass = request.scheme == 'https' ? URI::HTTPS : URI::HTTP
+    redirect_route = klass.build(host: request.host, port: request.port, path: path).to_s
+  end
+
+  def auth_hash
+    @_auth_hash ||= session.to_h.with_indifferent_access['dta.omniauth.auth']
+    session.delete('dta.omniauth.auth')
+    @_auth_hash
+  end
+
+  def default_devise_mapping
+    request.env['devise.mapping'].class_name.constantize
+  end
+
+  def resource_class
+    if omniauth_params[:resource_class]
+      omniauth_params[:resource_class].constantize
+    elsif params[:resource_class]
+      params[:resource_class].constantize
+    else
+      default_devise_mapping
     end
+  end
 
-    protected
+  def assign_provider_attrs(user, auth_hash)
+    attrs = auth_hash['info'].to_hash
+    attrs = attrs.slice(*user.attribute_names)
+    user.assign_attributes(attrs)
+  end
 
-
-    def auth_hash
-      request.env['omniauth.auth']
-    end
-
-    def default_devise_mapping
-      'User'.constantize
-    end
-
-
-    def resource_class
-      if omniauth_params[:resource_class]
-        omniauth_params[:resource_class].constantize
-      elsif params[:resource_class]
-        params[:resource_class].constantize
-      else
-        default_devise_mapping
-      end
-    end
-
-    def assign_provider_attrs(user, auth_hash)
-      attrs = auth_hash['info'].to_hash
-      attrs = attrs.slice(*user.attribute_names)
-      user.assign_attributes(attrs)
-    end
-
-    def whitelisted_params
-      whitelist = params_for_resource(:sign_up)
-      whitelist.each_with_object({}) do |key, coll|
-        param = omniauth_params[key.to_s]
-        coll[key] = param if param
-      end
+  def whitelisted_params
+    whitelist = params_for_resource(:sign_up)
+    whitelist.each_with_object({}) do |key, coll|
+      param = omniauth_params[key.to_s]
+      coll[key] = param if param
     end
   end
 end
--- spec/requests/api/user_can_login_with_spotify_spec.rb
+++ spec/requests/api/user_can_login_with_spotify_spec.rb
@@ -6,12 +6,11 @@ RSpec.describe 'GET /omniauth/spotify', type: :request do
   let(:json) { response_json }
   before do
     OmniAuth.config.test_mode = true
-    OmniAuth.config.before_callback_phase do |env|
-      env['omniauth.origin'] = 'https://example.com/'
-    end
     OmniAuth.config.mock_auth[:spotify] = OmniAuth::AuthHash.new(OmniAuthFixtures.spotify_response)
     Rails.application.env_config['devise.mapping'] = Devise.mappings[:user] # If using Devise
-    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:spotify]
+    allow_any_instance_of(ActionDispatch::Request)
+      .to receive(:session)
+      .and_return('dta.omniauth.auth': OmniAuth.config.mock_auth[:spotify])
   end
   describe 'WITH "resource_class" in params' do
     before do
@@ -34,35 +33,35 @@ RSpec.describe 'GET /omniauth/spotify', type: :request do
     }
     it {
       expect(headers)
-      .to have_key('access-token')
-      .and have_key('client')
-      .and have_key('token-type')
-      .and have_key('expiry')
-      .and have_key('uid')
-      .and have_key('spotify_credentials')
+        .to have_key('access-token')
+        .and have_key('client')
+        .and have_key('token-type')
+        .and have_key('expiry')
+        .and have_key('uid')
+        .and have_key('spotify_credentials')
     }
 
     it {
       expect(headers['spotify_credentials'])
-      .to have_key('token')
-      .and have_key('refresh_token')
-      .and have_key('expires_at')
-      .and have_key('expires')
+        .to have_key('token')
+        .and have_key('refresh_token')
+        .and have_key('expires_at')
+        .and have_key('expires')
     }
   end
 
   describe 'WITHOUT "resource_class" in params' do
     before do
-      Rails.application.env_config['omniauth.params'] = { }
+      Rails.application.env_config['omniauth.params'] = {}
       get '/auth/spotify/callback'
     end
 
     it { is_expected.to have_http_status 200 }
 
     it {
-      expect(json).to have_key('user').and have_key('spotify_info')
+      expect(json)
+        .to have_key('user')
+        .and have_key('spotify_info')
     }
-
   end
-
 end
