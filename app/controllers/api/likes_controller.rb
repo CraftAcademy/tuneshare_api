e5cd71edab583559a7b737f275a6fefe10e33525
Sanne <sannelindholm@hotmail.com>
2021-01-31 13:08:50 +0100

    Creating already_liked method in likes controller and adding sad path to test

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,12 +1,20 @@
 class Api::LikesController < ApplicationController
   def create
+    if already_liked?
+      render json: { message: "You can't like more than once" }, status: 422
+    else
     like = current_user.likes.create(like_params)
     render status: 201 if like.persisted?
+    end
   end
 
   private
 
   def like_params
     params.require(:like).permit(:user_id, :post_id)
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
   end
 end
--- spec/requests/api/user_can_like_another_post_spec.rb
+++ spec/requests/api/user_can_like_another_post_spec.rb
@@ -22,4 +22,25 @@ RSpec.describe 'POST /api/posts/:post_id/likes', type: :request do
       expect(existing_post.likes.count).to eq 1
     end
   end
+  describe 'unsuccessfully when user already liked the post' do
+    before do
+      2.times do
+        post "/api/posts/#{existing_post.id}/likes",
+             params: {
+               like: {
+                 post_id: existing_post.id
+               }
+             },
+             headers: user_header
+      end
+    end
+
+    it 'is expected to return a 422 status' do
+      expect(response).to have_http_status 422
+    end
+
+    it 'is expected to return an error message' do
+      expect(response_json['message']).to eq "You can't like more than once"
+    end
+  end
 end
