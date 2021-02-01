bcdc4c04a3c5a39c3137c7e025bed0ac756d11f8
viktor <viktorkarlsson125@gmail.com>
2021-02-01 20:26:12 +0100

    adding show action in user_controller with user_id and UserShowSerializer. generating UserShowSerializer with assosiation to PostIndexSerializer. test for user haveing email and post

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::UsersController < ApplicationController
   def show
-
+    user = User.find(params['id'])
+    render json: user, serializer: UserShowSerializer
   end
 end
--- spec/requests/api/user_can_view_their_own_profile_spec.rb
+++ spec/requests/api/user_can_view_their_own_profile_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe 'GET /api/users/:user_id', type: :request do
   let(:user) { create(:user) }
   let(:user_header) { user.create_new_auth_token }
+  let!(:post) { create(:post, user_id: user.id)}
 
   describe 'successfully view their profile' do
     before do
@@ -10,6 +11,14 @@ RSpec.describe 'GET /api/users/:user_id', type: :request do
 
     it 'is expected to return a 200 status' do
       expect(response).to have_http_status 200
+    end
+
+    it 'is expected to return a user email' do
+      expect(response_json["user"]["email"]).to eq "test-2@example.com"
+    end
+
+    it 'is expected to return posts of the user' do
+      expect(response_json["user"]["posts"][0]["track"]).to eq "MyTrack"
     end
   end
 end
