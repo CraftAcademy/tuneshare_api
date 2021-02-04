8346543eadde620a08b4c0ba0eed48d0571f453a
Sanne <sannelindholm@hotmail.com>
2021-02-04 10:12:35 +0100

    Adding destroy action in likes controller

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,11 +1,16 @@
 class Api::LikesController < ApplicationController
   def create
-    if already_liked?
-      render json: { message: "You can't like more than once" }, status: 422
-    else
+    # if already_liked?
+    #   render json: { message: "You can't like more than once" }, status: 422
+    # else
     like = current_user.likes.create(like_params)
-    render status: 201 if like.persisted?
-    end
+    render status: 201 
+    # end
+  end
+
+  def destroy
+   likes = current_user.likes.destroy(like_params) if already_liked?
+   render status: 200
   end
 
   private
