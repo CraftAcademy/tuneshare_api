7f84c81420eb3d9500472907271a9f31566be8a2
Kyungin Na <nakyungin93@gmail.com>
2021-02-05 09:43:28 +0100

    Adding destroy:dependent in post model and modifying user_can_delete_their_post_spec.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   validates_presence_of :track, :artists, :description
 
   belongs_to :user
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
