8dc50956c12f07779115dc4256be30df02252f1d
Kyungin Na <nakyungin93@gmail.com>
2021-02-05 14:09:03 +0100

    Modifying users in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-User.create(email: 'user1@mail.com', password: 'password')
-User.create(email: 'user2@mail.com', password: 'password')
-User.create(email: 'user3@mail.com', password: 'password')
-User.create(email: 'user4@mail.com', password: 'password')
+User.create(email: 'jackson.finch@gmail.com', password: 'password')
+User.create(email: 'viktor@karlsson.com', password: 'password')
+User.create(email: 'salindholm@gmail.com', password: 'password')
+User.create(email: 'rithreaksa@gmail.com', password: 'password')
+User.create(email: 'kyungin@gmail.com', password: 'password')
 
 Post.create(user_id: 1, track: 'Rosa helikopter', artists: 'Peaches', image: 'https://i.scdn.co/image/ab67616d0000b273d09ce4eeedc8c1c37323a587', preview: 'https://p.scdn.co/mp3-preview/76dc2316a985b89e1b5dd85334c54c2495caf2f9?cid=9165f2ed52ac4632b2c23038c2fbe1d9', description: 'I would love to fly in a pink helicopter! Anyone want to ride with me?')
 Post.create(user_id: 1, track: 'Papaya Whip', artists: 'American Rock Guitar Band', image: 'https://i.scdn.co/image/ab67616d0000b2738d928f59dec6b2bd601e5da3', preview: 'https://p.scdn.co/mp3-preview/75f676ea255a05b3f3714294b367a260a06dc31d?cid=9165f2ed52ac4632b2c23038c2fbe1d9', description: 'I have papayawhip for dessert today, it was so yummy!')
